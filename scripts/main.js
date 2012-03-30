442d651c4bdea2d9fd59a3ef04905362077d4d16
Elliot Lai <ellliotlai@gmail.com>
2012-03-31 02:54:42 +0800

    init

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+(function() {
+
+  require.config({
+    paths: {
+      jquery: 'lib/jquery-1.7.1.min',
+      io: 'lib/socket.io.min.amd',
+      raphael: 'lib/raphael-min',
+      underscore: 'lib/underscore-min.amd',
+      backbone: 'lib/backbone-min.amd',
+      hogan: 'lib/hogan-1.0.5.min.amd'
+    }
+  });
+
+  require(['jquery', 'io', 'raphael', 'underscore', 'backbone', 'hogan'], function($, io, Raphael, _, Backbone, hogan) {});
+
+}).call(this);
